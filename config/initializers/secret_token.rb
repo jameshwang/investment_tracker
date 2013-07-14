6a9f7691be9a4325b04cc81d24fcd6e5a8f61406
James Hwang <james.hwang.usc@gmail.com>
2013-07-14 14:16:05 -0700

    Init with User scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InvestmentTracker::Application.config.secret_key_base = '66966ff5025394e3b21e4cb26af6122bf6fb86adb85b06f68b17d8ff0bd9601f83452b8f90770e45193e62da8f90eecc7dd8dc6c70590690022cba7f424acf15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InvestmentTracker::Application.config.session_store :cookie_store, key: '_investment_tracker_session'
